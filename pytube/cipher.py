5066725e1cb520ab0083719ffebb34508bc1c1a3
LukasNel <lukasabrie@gmail.com>
2023-07-19 23:29:06 -0500

    Update cipher.py

--- pytube/cipher.py
+++ pytube/cipher.py
@@ -224,7 +224,8 @@ def get_transform_object(js: str, var: str) -> List[str]:
     regex = re.compile(pattern, flags=re.DOTALL)
     transform_match = regex.search(js)
     if not transform_match:
-        raise RegexMatchError(caller="get_transform_object", pattern=pattern)
+        pass
+        #raise RegexMatchError(caller="get_transform_object", pattern=pattern)
 
     return transform_match.group(1).replace("\n", " ").split(", ")
 
